9f665820e1ed5da82d8786127b88149b4dca43c1
DHLAU <lauduanhang@gmail.com>
2025-05-08 00:09:33 +0800

    Yay

--- db.js
+++ db.js
@@ -1,8 +1,7 @@
 const { MongoClient } = require('mongodb')
 
 let dbConnection
-const URL = 'mongodb+srv://lauduanhang:qwer1234@cluster0.dadg8gh.mongodb.net/Pickify?retryWrites=true&w=majority&appName=Cluster0'
-
+const URL = process.env.MONGO_URL
 
 module.exports = {
     connectToDb: (cb) => {
