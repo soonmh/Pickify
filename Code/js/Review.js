0e4939a69d81ac64f6c46759d76ac314915f9522
Kejia1125 <149474613+Kejia1125@users.noreply.github.com>
2025-06-14 14:25:26 +0800

    update db.js

--- Code/js/Review.js
+++ Code/js/Review.js
@@ -1,10 +1,30 @@
 const mongoose=require('mongoose');
 
 const reviewSchema=new mongoose.Schema({
+    entertainmentId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'Entertainment',
+        required:true
+    },
     user:String,
     rating:Number,
     text:String,
-    timeStamp:Number
-})
+    timeStamp:{
+        type:Date,
+        default:Date.now
+    },
+    comments:[{
+        user:String,
+        comment:String,
+        timestamp:{
+            type:Date,
+            default:Date.now
+        }
+    }],
+    reported:{
+        type:Boolean,
+        default:false
+    }
+});
 
 module.exports=mongoose.model('Review',reviewSchema);
